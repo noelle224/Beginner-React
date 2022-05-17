eac382b92379c560feb0702b620ff469183c2a09
noelle224 <bhavika.bharti17@gmail.com>
2022-05-16 21:38:02 -0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='Header'>
+        <header>
+            <h1>To Do List</h1>
+        </header>
+        </div>
+    );
+};
+
+export default Header;
